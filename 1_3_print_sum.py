9c2a7ee72acb70b28ea57f58d4a8ba879e933dcc
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:32:54 +0300

    added some sentences

--- 1_3_print_sum.py
+++ 1_3_print_sum.py
@@ -4,4 +4,4 @@ num2 = int(input("Enter a value for number 2: "))
 # perform arithmetic operation (addition)
 sum = num1 + num2
 # print result
-print(sum)
+print(f"The sum of these numbers is: {sum}")
