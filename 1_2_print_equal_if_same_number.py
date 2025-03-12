d905ffa71fe505b68575bfef791a8b0926f1f2c0
Andre <andrepetilunalolong@gmail.com>
2025-03-12 14:08:04 +0800

    print equal (final)

---
+++ 1_2_print_equal_if_same_number.py
@@ -0,0 +1,9 @@
+# ask the user for two numbers
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: ")) 
+# chech if equal
+# print if true
+if num1 == num2:
+    print("Equal")
+else:
+    print("Entered numbers are not equal")
