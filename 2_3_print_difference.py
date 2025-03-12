ed5efae8cf53467c518a18f100cd966de4cb3e54
Andre <andrepetilunalolong@gmail.com>
2025-03-12 11:23:36 +0300

    print differences

--- 2_3_print_difference.py
+++ 2_3_print_difference.py
@@ -1,3 +1,12 @@
 # ask the user for numbers
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
+
 # perform arithmetic operation (subtraction)
+dif1 = num1 - num2
+dif2 = num2 - num1
+
 # print difference
+print(f"First number minus second number is {dif1}")
+print("and")
+print(f"The second number divided by the first number is {dif2}")
