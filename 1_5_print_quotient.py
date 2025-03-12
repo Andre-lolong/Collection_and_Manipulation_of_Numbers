a625d035c35e556e6b8f300d4427334ed054b22f
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:43:50 +0300

    print quotient

---
+++ 1_5_print_quotient.py
@@ -0,0 +1,21 @@
+# ask user for number
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
+
+# perfrom arithmetic operation (division)
+# check for undefined answer\
+# print quotient
+if  num1 != 0 and num2 != 0:
+    print(f"The first number divided by the second number will resuslt to {num1/num2}\n")
+    print("and")
+    print(f"The second number divided by the first number will resuslt to {num2/num1}")
+if num1 != 0 and num2 == 0:
+    print("The first number divided by the second number will be undefined")
+    print("and")
+    print("The second number divided by the first number will resuslt to zero")
+if num1 == 0 and num2 != 0:
+    print("The first number divided by the second number will resuslt to zero")
+    print("and")
+    print("The second number divided by the first number will be undefined")
+if num1 == 0 and num2 == 0:
+    print("This operation is unforgivable")
