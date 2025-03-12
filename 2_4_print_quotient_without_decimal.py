8bd5a84848ca0f46e3087b3d018dfc20b53771e6
Andre <andrepetilunalolong@gmail.com>
2025-03-12 11:39:59 +0300

    print quotient without decimal

--- 2_4_print_quotient_without_decimal.py
+++ 2_4_print_quotient_without_decimal.py
@@ -1,4 +1,23 @@
 # ask user for numbers
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
+
 # perfrom arithmetic operation (division)
-# check for undefined answer\
-# print quotient
+# check for undefined answer
+# print quotient without decimal
+if  num1 != 0 and num2 != 0:
+    print(f"The first number divided by the second number will resuslt to {num1//num2}")
+    print("and")
+    print(f"The second number divided by the first number will resuslt to {num2//num1}")
+elif num1 != 0 and num2 == 0:
+    print("The first number divided by the second number will be undefined")
+    print("and")
+    print("The second number divided by the first number will resuslt to zero")
+elif num1 == 0 and num2 != 0:
+    print("The first number divided by the second number will resuslt to zero")
+    print("and")
+    print("The second number divided by the first number will be undefined")
+elif num1 == 0 and num2 == 0:
+    print("This operation is unforgivable")
+else:
+    print("Try again")
