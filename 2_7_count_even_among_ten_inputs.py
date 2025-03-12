a7f814f4e6ab8c27b8f9abb9b52661073b3ef9f9
Andre <andrepetilunalolong@gmail.com>
2025-03-12 12:09:33 +0300

    fixed the pseudocode

--- 2_7_count_even_among_ten_inputs.py
+++ 2_7_count_even_among_ten_inputs.py
@@ -1,5 +1,3 @@
-# use for loop for 1-100
-for i in range(101):
-    if i % 2 == 0: # identify even
-        print(i) # print even
-
+# ask the user for numbers
+# count even numbers
+# print how many even
