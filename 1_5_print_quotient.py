a05c4b0ccc649b951ebb264e53477a1785193767
Andre <andrepetilunalolong@gmail.com>
2025-03-12 11:37:18 +0300

    fixed the if and elif

--- 1_5_print_quotient.py
+++ 1_5_print_quotient.py
@@ -9,15 +9,15 @@ if  num1 != 0 and num2 != 0:
     print(f"The first number divided by the second number will resuslt to {num1/num2}\n")
     print("and")
     print(f"The second number divided by the first number will resuslt to {num2/num1}")
-if num1 != 0 and num2 == 0:
+elif num1 != 0 and num2 == 0:
     print("The first number divided by the second number will be undefined")
     print("and")
     print("The second number divided by the first number will resuslt to zero")
-if num1 == 0 and num2 != 0:
+elif num1 == 0 and num2 != 0:
     print("The first number divided by the second number will resuslt to zero")
     print("and")
     print("The second number divided by the first number will be undefined")
-if num1 == 0 and num2 == 0:
+elif num1 == 0 and num2 == 0:
     print("This operation is unforgivable")
 else:
     print("Try again")
