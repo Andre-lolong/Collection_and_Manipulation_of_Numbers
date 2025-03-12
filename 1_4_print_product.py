04431da913784ccf15308eb22a77de9e011afe23
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:35:35 +0300

    print product

--- 1_4_print_product.py
+++ 1_4_print_product.py
@@ -1,3 +1,7 @@
 # ask user for number
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
 # perform arithmetic operation (multiplication) 
+product = num1 * num2 
 # print product
+print(f"The product of these numbers is {product}")
