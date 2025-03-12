ab3a81da1357fff056f1001c24c91ca95bc83a5f
Andre <andrepetilunalolong@gmail.com>
2025-03-12 12:13:14 +0300

    counts of even numbers from ten inputs

--- 2_7_count_even_among_ten_inputs.py
+++ 2_7_count_even_among_ten_inputs.py
@@ -1,3 +1,9 @@
 # ask the user for numbers
-# count even numbers
+even = 0 
+for i in range(10):
+    num = int(input(f"Enter a value for number {i + 1}: "))
+    if i % 2 == 0: 
+        even += 1 # count even numbers
+
 # print how many even
+print(f"The even numbers entered are: {even}")
