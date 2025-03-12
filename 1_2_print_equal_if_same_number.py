ac409cac6817843d746bf944036f58476cf89c26
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:42:22 +0300

    added spaces

--- 1_2_print_equal_if_same_number.py
+++ 1_2_print_equal_if_same_number.py
@@ -1,6 +1,7 @@
 # ask the user for two numbers
 num1 = int(input("Enter a value for number 1: "))
 num2 = int(input("Enter a value for number 2: ")) 
+
 # chech if equal
 # print if true
 if num1 == num2:
