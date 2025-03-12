7354e23f5374a015561c37985a19c86ffa3d99e8
Andre <andrepetilunalolong@gmail.com>
2025-03-12 08:47:57 +0300

    print the bigger number (final)

--- 1_1_print_bigger_number.py
+++ 1_1_print_bigger_number.py
@@ -5,8 +5,8 @@ num2 = int(input("Enter a value for number 2: "))
 # verify and evaluate inputs 
 # print bigger number
 if num1 > num2:
-    print(f"The bigger number is number 1 which is: {num1}")
-if num2 > num1:
-    print(f"The bigger number is number 2 which is: {num2}")
+    print(f"The bigger number is the first which is: {num1}")
+elif num2 > num1:
+    print(f"The bigger number is the second which is: {num2}")
 else:
-    print("The values are the same")
+    print("The values entered are the same.")
