002862e79ff8bee08050308bb3bb56e0ccbe70b0
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:43:17 +0300

    added spaces

--- 1_4_print_product.py
+++ 1_4_print_product.py
@@ -1,7 +1,9 @@
 # ask user for number
 num1 = int(input("Enter a value for number 1: "))
 num2 = int(input("Enter a value for number 2: "))
+
 # perform arithmetic operation (multiplication) 
-product = num1 * num2 
+product = num1 * num2
+ 
 # print product
-print(f"The product of these numbers is {product}")
+print(f"The product of these numbers is: {product}")
