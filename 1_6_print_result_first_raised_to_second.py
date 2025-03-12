c8a5f2881c6b880b5d041c2d49c30ea656e155c2
Andre <andrepetilunalolong@gmail.com>
2025-03-12 14:48:51 +0800

    print the result

---
+++ 1_6_print_result_first_raised_to_second.py
@@ -0,0 +1,7 @@
+# ask the user for number
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
+# raise the first number to second number
+square = num1 ** num2
+# print result
+print(f"The first number raised to the second number is: {square}")
