f2efa2f3b988421e599c5ee81392e6d275ed3c0c
Andre <andrepetilunalolong@gmail.com>
2025-03-12 10:12:41 +0300

    odd count

--- 1_8_odd_count_among_ten_numbers.py
+++ 1_8_odd_count_among_ten_numbers.py
@@ -1,3 +1,9 @@
 # ask user for ten numbers 
-# ientify which is odd
+total = 0
+for i in range(10):
+    num = int(input(f"Enter a value for number {i + 1}: "))
+    if num % 2 != 0: # ientify which is odd
+        total += num
+
 # count the odds and print
+print(f"The odd number counnt is: {total}")
