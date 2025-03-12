cf5a7bfa8303f32a661f3704dd2e8a65461b3eb7
Andre <andrepetilunalolong@gmail.com>
2025-03-12 16:09:27 +0300

    milestone

--- 3_2_display_all_but_only_one_for_duplicated.py
+++ 3_2_display_all_but_only_one_for_duplicated.py
@@ -1,12 +1,16 @@
 # ask user for numbers
-numbers = []
-seen = set()
-filtered_numbers = []
-
 def display_filtered_numbers():
+    numbers = []
+    seen = set()
+    filtered_numbers = []
+    
     for i in range(10):
         num = int(input(f"Enter a value for number {i + 1}: "))
         numbers.append(num)
+    if num not in seen: # check for duplicates
+        seen.add(num)
+        filtered_numbers.append(num)
 
-# check for duplicates
-# display all numbers but only first entry for duplicated numbers
+    print(f"Here are the numbers that are filtered: {filtered_numbers} ")# display all numbers but only first entry for duplicated numbers
+
+display_filtered_numbers()
