da0d59af32a9b781022eaf9d5187ce5e8b18aee5
Andre <andrepetilunalolong@gmail.com>
2025-03-12 09:42:58 +0300

    added spaces

--- 1_3_print_sum.py
+++ 1_3_print_sum.py
@@ -1,7 +1,9 @@
 # ask the user for two numbers
 num1 = int(input("Enter a value for number 1: "))
 num2 = int(input("Enter a value for number 2: "))
+
 # perform arithmetic operation (addition)
 sum = num1 + num2
+
 # print result
 print(f"The sum of these numbers is: {sum}")
