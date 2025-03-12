b88ee520c05b94044064501d5f73ace344b04d66
Andre <andrepetilunalolong@gmail.com>
2025-03-12 10:40:04 +0300

    print not equal

---
+++ 2_2_print_not_equal_if_not_same.py
@@ -0,0 +1,10 @@
+# ask user for numbers 
+num1 = int(input("Enter a value for number 1: "))
+num2 = int(input("Enter a value for number 2: "))
+
+# identify if not equal
+# print not equal if not equal 
+if num1 != num2:
+    print("Not Equal")
+else:
+    print("Entered numbers are equal")
